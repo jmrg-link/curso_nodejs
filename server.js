dbd09460e0510c394d7709b149a4d8f1acacf01b
Jesus Mª Rico Gonzalez <52455810+jmrg-link@users.noreply.github.com>
2021-09-26 19:21:37 +0200

    inicio basico explicacion router

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require ('express')
 
 const app = express()
+const router = express.Router()
+app.use(router)
 
-app.use ('/' , (req,res) =>{
-    res.send('hi')
+app.get('/',(req,res)=>{
+    res.send('hola desde get')
 })
+app.post('/',(req,res)=>{
+    res.send('msg added')
+})
+//app.use ('/' , (req,res) =>{
+//    res.send('hi')
+//})
 
 app.listen(3000)
 console.log('Server runninig port 3000')
