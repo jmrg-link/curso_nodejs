22ce80622c14b16d216dc75210290c9354397670
Jesus Mª Rico Gonzalez <52455810+jmrg-link@users.noreply.github.com>
2021-09-26 19:41:53 +0200

    inicio basico explicacion req.body req.query

--- server.js
+++ server.js
@@ -2,17 +2,26 @@ const express = require ('express')
 
 const app = express()
 const router = express.Router()
+
+app.use(express.json());
 app.use(router)
 
-app.get('/',(req,res)=>{
-    res.send('hola desde get')
+app.get('/message',(req,res)=>{
+    res.send('Lista de msg')
 })
-app.post('/',(req,res)=>{
-    res.send('msg added')
+
+app.post('/message',(req,res)=>{
+    console.log(req.body)
+    res.send('msg añadido correctamente')
+
 })
-//app.use ('/' , (req,res) =>{
-//    res.send('hi')
-//})
+
+app.delete('/message',(req,res)=>{
+    console.log(req.body)
+    console.log(req.query)
+    res.send(`Mensaje ${req.body.text} añadido correctamente` )
+
+})
 
 app.listen(3000)
 console.log('Server runninig port 3000')
